731c57e921b4e2ada3e0e5454ce5793934d7156c
jbshipman <jbshipman@icloud.com>
2020-03-03 09:27:24 -0600

    built model, assoc.,

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :school
+  has_many :appointments
+  has_many :tutors, through: :appointments
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,5 @@
 class Tutor < ApplicationRecord
   belongs_to :school
+  has_many :appointments
+  has_many :students, through: :appointments
 end
